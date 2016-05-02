abf1fe0e9dfd454f2b2fa1bc35f4eecb0d2eb82b
olodev <realmarra@gmail.com>
2016-05-02 13:42:26 +0200

    License

--- Breakout/Game1.cs
+++ Breakout/Game1.cs
@@ -1,4 +1,24 @@
-﻿using Microsoft.Xna.Framework;
+﻿/*
+
+    Simple Breakout Game made in C# using the MonoGame SDK
+    Copyright (C) 2016  Marcel Kurz
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
